a129887cb20ab82546ff1940f366601580a04b84
Brittany Fisher-Morris <bmmf@bms-imac.attlocal.net>
2019-09-19 23:25:57 -0400

    initial commit, person class created

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,11 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(info){
+        this.name = info.name,
+        this.age = info.age,
+        this.location = info.location
+    }
+    speak(){
+        `Hello my name is ${this.name} and I am from ${this.location}`;
+    }
+}
