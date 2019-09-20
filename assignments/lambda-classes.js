d14b2619580dc64986873b52d06283c0450e33e1
Brittany Fisher-Morris <bmmf@bms-imac.attlocal.net>
2019-09-20 00:11:10 -0400

    new objects complete

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -45,7 +45,7 @@ class Student extends Instructor{
     }
 }
 
-class TeamLeads extends Student{
+class TeamLead extends Student{
     constructor(pm){
         super(pm);
         this.gradClassName = pm.gradClassName,
@@ -58,3 +58,67 @@ class TeamLeads extends Student{
         return `${this.name} debugs ${student.name}'s code on ${subject}`;
     }
 }
+
+////////////////////
+const darkLord = new Instructor({
+    name: "Voldemort",
+    location: "Wiltshire",
+    age: 71,
+    favLanguage: "Parseltongue",
+    specialty: "Dark Arts",
+    catchPhrase: "Avada Kedavra"
+  });
+  
+  const yoda = new Instructor({
+    name: "Yoda",
+    location: "Dagobah",
+    age: 900,
+    favLanguage: "Object-Subject-Verb",
+    specialty: "Using the force",
+    catchPhrase: "Powerful you have become, the dark side I sense in you"
+  });
+  
+  const awesome = new Student({
+    name: "Brittany",
+    location: "Louisville",
+    age: 30,
+    gender: "female",
+    previousBackground: "stocks",
+    className: "Web24",
+    favSubjects: ["Aliens", "Magic", "JavaScript...I guess"]
+  });
+  
+  const mudblood = new Student({
+    name: "Hermione",
+    location: "Hogwarts",
+    age: 18,
+    gender: "female",
+    previousBackground: "muggle things",
+    className: "Gryffindor'",
+    favSubjects: ["Charms", "Transfiguration", "Defense Against the Dark Arts"]
+  });
+  
+  const prettyFlyForATLGuy = new TeamLead({
+    name: "Will VanOrder",
+    location: "Inside the monitor",
+    age: "ain't nuthin but a # ",
+    gender: "male",
+    favLanguage: "Javascript",
+    specialty: "All things",
+    catchPhrase: "Let’s git’r done",
+    gradClassName: "Web20.75",
+    favInstructor: "Dan Levy"
+  });
+  
+  const elon = new TeamLead({
+    name: "Elon Musk",
+    location: "Tesla",
+    age: 47,
+    gender: "male",
+    favLanguage: "C++",
+    specialty: "Back-end",
+    catchPhrase:
+      "There certainly will be job disruption. Because what’s going to happen is robots will be able to do everything better than us. ... I mean all of us",
+    gradClassName: "We don't need no education",
+    favInstructor: "life"
+  });
